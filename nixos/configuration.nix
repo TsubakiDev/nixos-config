c7b423ea45ddec18cd612ae5fe1db5966049301a
TsubakiDev <i@tsubakidev.cc>
2024-06-18 17:08:37 +0800

    up

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,7 @@
         ../../common/input.nix
         ../../common/sound.nix
         ../../common/fonts.nix
+        ../../common/internationalisation.nix
         ../../common/settings/nix.nix
     ];
 
@@ -27,21 +28,6 @@
 
     # Set your time zone.
     time.timeZone = "Asia/Shanghai";
-
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "zh_CN.UTF-8";
-        LC_IDENTIFICATION = "zh_CN.UTF-8";
-        LC_MEASUREMENT = "zh_CN.UTF-8";
-        LC_MONETARY = "zh_CN.UTF-8";
-        LC_NAME = "zh_CN.UTF-8";
-        LC_NUMERIC = "zh_CN.UTF-8";
-        LC_PAPER = "zh_CN.UTF-8";
-        LC_TELEPHONE = "zh_CN.UTF-8";
-        LC_TIME = "zh_CN.UTF-8";
-    };
 
     # Enable CUPS to print documents.
     services.printing.enable = true;
