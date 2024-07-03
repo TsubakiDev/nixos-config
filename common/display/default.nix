79aa0ff8ba0ead283417429a08a3e4a96e149a9c
TsubakiDev <i@tsubakidev.cc>
2024-07-04 00:31:19 +0800

    up

--- common/display/default.nix
+++ common/display/default.nix
@@ -1,46 +1,48 @@
 { pkgs, ... }:
 
 {
-    services = {
-		# Ensure gnome-settings-daemon udev rules are enabled.
-		udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-		xserver = {
-			# Required for DE to launch.
-			enable = true;
-			displayManager = {
-				gdm = {
-					enable = true;
-					#wayland = true;
-				};
-			};
-			# Enable Desktop Environment.
-			desktopManager.gnome.enable = true;
-			# Configure keymap in X11.
-			xkb = {
-				layout = "us";
-				variant = "";
-			};
-			# Exclude default X11 packages I don't want.
-			excludePackages = with pkgs; [ xterm ];
-		};
-	};
+  services = {
+    # Ensure gnome-settings-daemon udev rules are enabled.
+    udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+    xserver = {
+      # Required for DE to launch.
+      enable = true;
+      displayManager = {
+        gdm = {
+          enable = true;
+          #wayland = true;
+        };
+      };
+      # Enable Desktop Environment.
+      desktopManager.gnome.enable = true;
+      # Configure keymap in X11.
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      # Exclude default X11 packages I don't want.
+      excludePackages = with pkgs; [ xterm ];
+    };
+  };
 
-    environment.gnome.excludePackages = (with pkgs; [
-        gnome-photos
-        gnome-tour
-        gedit
-    ]) ++ (with pkgs.gnome; [
-        cheese # webcam tool
-        gnome-music
-        gnome-terminal
-        epiphany # web browser
-        geary # email reader
-        evince # document viewer
-        gnome-characters
-        totem # video player
-        tali # poker game
-        iagno # go game
-        hitori # sudoku game
-        atomix # puzzle game
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gedit
+    ])
+    ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      gnome-terminal
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
     ]);
 }
