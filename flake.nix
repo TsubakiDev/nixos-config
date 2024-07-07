a00a758d2f945f69c685892343a9f95364d83b63
TsubakiDev <i@tsubakidev.cc>
2024-07-07 17:29:52 +0800

    gaming now!

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
   inputs = {
+    # Utils
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Gaming
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
       self,
       nixpkgs,
       home-manager,
+      aagl,
       ...
     }@inputs:
     {
