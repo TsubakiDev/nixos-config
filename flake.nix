a563c9a47c0d114dfb4c29aa7790d838cccae6a0
TsubakiDev <i@tsubakidev.cc>
2024-07-07 20:32:48 +0800

    fix aagl

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
